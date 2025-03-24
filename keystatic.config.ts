628a6b024052d181935199e317d029da90666595
pavanbhaskar <pavanbhaskardev@gmail.com>
2025-03-24 22:33:24 +0530

    chore: updated config

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,25 +1,52 @@
-import { config, fields, collection } from '@keystatic/core';
+import { config, fields, collection } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: "github",
+    repo: "pavanbhaskardev/keystatic",
+    pathPrefix: "prod",
   },
   collections: {
     posts: collection({
-      label: 'Posts',
-      slugField: 'title',
-      path: 'src/content/posts/*',
-      format: { contentField: 'content' },
+      label: "Posts",
+      slugField: "title",
+      path: "src/content/posts/*",
+      format: { contentField: "content" },
       schema: {
-        title: fields.slug({ name: { label: 'Title' } }),
+        title: fields.slug({ name: { label: "Title" } }),
         content: fields.markdoc({
-          label: 'Content',
+          label: "Content",
           options: {
             image: {
-              directory: 'src/assets/images/posts',
-              publicPath: '../../assets/images/posts/',
+              directory: "src/assets/images/posts",
+              publicPath: "../../assets/images/posts/",
             },
           },
+        }),
+        author: fields.relationship({
+          label: "Author",
+          collection: "authors",
+          validation: {
+            isRequired: true,
+          },
+        }),
+      },
+    }),
+    authors: collection({
+      label: "Authors",
+      slugField: "name",
+      path: "src/content/authors/*",
+      format: { data: "yaml", contentField: "markdoc" },
+      schema: {
+        markdoc: fields.emptyContent({
+          extension: "mdoc",
+        }),
+        name: fields.slug({ name: { label: "Name" } }),
+        bio: fields.text({ label: "Bio" }),
+        avatar: fields.image({
+          label: "Avatar",
+          directory: "public/images/authors",
+          publicPath: "/images/authors",
         }),
       },
     }),
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,21 @@
 // @ts-ignore
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const posts = defineCollection({
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
+    author: z.string(),
   }),
 });
 
-export const collections = { posts };
+const authors = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    name: z.string(),
+    bio: z.string(),
+    avatar: z.string(),
+  }),
+});
+
+export const collections = { posts, authors };
